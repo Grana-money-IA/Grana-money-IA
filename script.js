c4dd248bf97f343b93562b1d54b3c191b7ed972d
Grana-money-IA <vilelaalisson325@gmail.com>
2025-11-06 15:49:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const alvo = document.querySelector(this.getAttribute('href'));
+    if (alvo) alvo.scrollIntoView({ behavior: 'smooth' });
+  });
+});
